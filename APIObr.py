2eafbea9c78d21fb1134e45a7bce30dfd3297bc3
ernestis-1 <e.ivanovernest@yandex.ru>
2023-12-12 00:08:32 +0300

    mistake2

--- APIObr.py
+++ APIObr.py
@@ -3,19 +3,16 @@ import requests
 url_adr = 'https://api.tracker.yandex.net/v2/issues'
 
 headers = {
-    'Authorization': 'OAuth y0_AgAAAAA5ozOkAAr5RAAAAAD0hgNx0WF7dIF2QaKhs4lJ4XonLTn8x0U',
+    'Authorization': 'OAuth y0_AgAAAAA5ozOkAAr5RAAAAAD0hryjLDTbl6_eTX2W63pfrtAH6L2bz34',
     'X-Cloud-Org-ID': 'bpf10vaclnlj4tnvv9ko',
     'Content-Type': 'application/json'
 }
 
 data = {
     'queue': 'TEAMCITYBUILDFA',
-    'summary': 'BUILD IS REALLY DOWN',
+    'summary': 'BUILD REALLY FAIL BRO',
     'assignee': 'e.ivanovernest'
 }
 
-
-
-result = requests.post(url_adr, headers=headers, json=data)
-print(result)
-
+response = requests.post(url_adr, headers=headers, json=data)
+print(response)
